d99e07cda0272090a9c996c4e37f87dabbbf83bf
kaibashira hodade <hodade@gmail.com>
2014-07-09 23:18:07 +0900

    初回コミット

---
+++ colorlabel.js
@@ -0,0 +1,66 @@
+//checkbox,radio チェック状態でラベルの色変え by tanaka
+(function($) {
+
+  var methods = {
+    init : function( options ) {
+
+      //チェック済みの背景色を変える
+      $('input[type="checkbox"],input[type="radio"]').each(function(){
+        if ($(this).prop('checked')) {
+          $(this).parent('label').addClass('colorlabel_active');
+        } else {
+          $(this).parent('label').removeClass('colorlabel_active');
+        }
+      });
+
+      //チェックボックスへのイベントバインド
+      $('input[type="checkbox"]').bind('click', function(){
+        if ($(this).prop('checked')) {
+          $(this).parent('label').addClass('colorlabel_active');
+        } else {
+          $(this).parent('label').removeClass('colorlabel_active');
+        }
+      });
+
+      //ラジオボタンへのイベントバインド
+      $('input[type="radio"]').bind('click', function(){
+        var name = $(this).attr('name');
+        $('input[name='+name+']').each(function(){
+          if ($(this).prop('checked')) {
+            $(this).parent('label').addClass('colorlabel_active');
+          } else {
+            $(this).parent('label').removeClass('colorlabel_active');
+          }
+        });
+      });
+
+      //リセットボタンへのイベントバインド
+      $('input[type="reset"]').bind('click', function(){
+        $('input[type="checkbox"],input[type="radio"]').each(function(){
+          if ($(this).prop('checked')) {
+            $(this).parent('label').addClass('colorlabel_active');
+          } else {
+            $(this).parent('label').removeClass('colorlabel_active');
+          }
+        });
+      });
+
+    }
+  };
+
+  $.fn.colorlabel = function( method ) {
+    // メソッド呼び出し部分
+    if ( methods[method] ) {
+      return methods[ method ].apply( this, Array.prototype.slice.call( arguments, 1 ));
+    } else if ( typeof method === 'object' || ! method ) {
+      return methods.init.apply( this, arguments );
+    } else {
+      $.error( 'Method ' +  method + ' does not exist on jQuery.colorlabel' );
+    }
+  };
+
+  $(window).load(function () {
+      methods.init.apply();
+  });
+
+})(jQuery);
